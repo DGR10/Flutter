26cabbad16b70087ffe6e0f1405cddbb940aa510
Carlos Guijarro Martín <carlosguijarromartin@gmail.com>
2020-12-02 22:06:23 +0100

    Avatar

--- componentes/lib/src/pages/avatar.page.dart
+++ componentes/lib/src/pages/avatar.page.dart
@@ -7,6 +7,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://as.com/meristation/imagenes/2018/12/18/noticias/1545127005_349047_1545127139_noticia_normal.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('CG'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://media.redadn.es/imagenes/the-legend-of-zelda-breath-of-the-wild-nintendo-switch-wii-u_328448.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration( milliseconds: 1000 ),
+        ),
       ),
     );
   }
