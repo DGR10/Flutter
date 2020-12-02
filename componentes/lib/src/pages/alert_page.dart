f92b1975c80adaefc01319d962f19d6147accbc2
Carlos Guijarro Martín <carlosguijarromartin@gmail.com>
2020-12-02 21:50:40 +0100

    AlertPage

--- componentes/lib/src/pages/alert_page.dart
+++ componentes/lib/src/pages/alert_page.dart
@@ -8,12 +8,52 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () =>_mostrarAlert(context)),
+      ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.logout),
+        child: Icon(Icons.close),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
     );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+
+    showDialog(
+      context: context,
+      barrierDismissible: false, // Para que se pueda cerrar la alerta pinchando fuera => true
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder( borderRadius:BorderRadius.circular(20.0)),
+          title: Text('Title'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido de lacaja de la alerta'),
+              FlutterLogo( size: 100.0,)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(), 
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () => Navigator.of(context).pop(), 
+            ),
+          ],
+        );
+      }
+    );
+
   }
 }
